71b48635f0c2f83ef50112fb17c6c6483e8f862d
rashed <x.suwaidi@gmail.com>
2022-11-23 14:55:11 +0400

    update botton

--- src/components/Form.js
+++ src/components/Form.js
@@ -35,7 +35,7 @@ export default function FlyerForm(props) {
           <div className="px-4 py-3 text-right sm:px-6">
             <button
               type="submit"
-              className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               Submit
             </button>
